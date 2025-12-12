8c7f10c5e8c35618feefa842f2f68c8da59b2b3d
Wit Krittawit <wit@zervi.com>
2025-12-12 10:59:48 +0700

    Add hide cost price

---
+++ hide_show_cost_price_knk/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Powered by Kanak Infosystems LLP.
+# © 2020 Kanak Infosystems LLP. (<https://www.kanakinfosystems.com>).
+
+{
+    'name': 'Hide/Show Cost Price',
+    'version': '19.0.1.0',
+    'category': 'Tools',
+    'depends': ['product', 'sale_management'],
+    'license': 'OPL-1',
+    'author': 'Kanak Infosystems LLP.',
+    'website': 'https://www.kanakinfosystems.com',
+    'summary': 'Hide and show cost price in products',
+    'description': """
+         Hide and show cost price in products| hide | show | cost |price | cost price | hide price | show price|
+    """,
+    'data': [
+        'security/security.xml',
+        'views/product_views.xml',
+        'views/product_template_views.xml',
+    ],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+}
