cf61ecefd780d9e386c43d8dff9daa428160b840
Wit Krittawit <wit@zervi.com>
2026-04-03 08:14:55 +0700

    Fixed error code

--- api_module/models/__init__.py
+++ api_module/models/__init__.py
@@ -1,7 +1,7 @@
 from . import api_config
 from . import sync_job
 from . import api_token
-from . import stock
+# from . import stock
 from . import table
 from . import product
 from . import teable
